73551eaecf4547aac3183bef90273bd8b933dfad
Jose Andres Herrera Rincon <140762626+Jahrming@users.noreply.github.com>
2025-08-12 18:10:37 -0500

    Add .next/static/chunks/src_app_comunidad_page_tsx_1135c602._.js 1755040237362

---
+++ .next/static/chunks/src_app_comunidad_page_tsx_1135c602._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_480c925c._.js",
+  "static/chunks/node_modules_6a7209d3._.js"
+],
+    source: "dynamic"
+});
