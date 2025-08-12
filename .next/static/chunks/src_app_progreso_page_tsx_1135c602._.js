536d74f7dbceb569500634305b11b6decfd778f4
Jose Andres Herrera Rincon <140762626+Jahrming@users.noreply.github.com>
2025-08-12 18:10:52 -0500

    Add .next/static/chunks/src_app_progreso_page_tsx_1135c602._.js 1755040251519

---
+++ .next/static/chunks/src_app_progreso_page_tsx_1135c602._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_bb6f9cd2._.js",
+  "static/chunks/node_modules_lodash_ff39e716._.js",
+  "static/chunks/node_modules_recharts_es6_80d295e7._.js",
+  "static/chunks/node_modules_c133a428._.js"
+],
+    source: "dynamic"
+});
