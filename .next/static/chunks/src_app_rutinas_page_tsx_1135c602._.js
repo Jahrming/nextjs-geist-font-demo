ad1723f1427d43f20da17dc87277ba6a702e4b28
Jose Andres Herrera Rincon <140762626+Jahrming@users.noreply.github.com>
2025-08-12 18:13:10 -0500

    Add .next/static/chunks/src_app_rutinas_page_tsx_1135c602._.js 1755040389369

---
+++ .next/static/chunks/src_app_rutinas_page_tsx_1135c602._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_3bc57458._.js",
+  "static/chunks/node_modules_98fc6db4._.js"
+],
+    source: "dynamic"
+});
